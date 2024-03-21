512ba71123c1af474b5786f26f1103cc9aaa547f
stilyan91 <stilyan91@gmail.com>
2024-03-21 18:00:05 +0200

    modified gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
+#include <stdio.h>
 
+#include <stdlib.h>
 
+struct Rectangle {
+
+	int lenght;
+	int breadth;
+	char x;
+};
 int main() {
 
-	std::cout << "Hello World!" << std::endl;
-	
+	int* p;
+	p = (int*) malloc(5 * sizeof(int)); // C
+
+	p = new int[5]; // c++
+
 	return 0;
 }
